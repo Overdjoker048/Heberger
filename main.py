61fc5e0a988a550ac8f3b8c734a695009b131c1e
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2023-01-21 22:27:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
-from os import system
-from time import sleep
+from subprocess import run
+from requests import get
 
-system("color a")
-system("python -m pip install -r requirements.txt")
-system("python -m pip install --upgrade pip")
-system("cls")
-
-print(""" /$$   /$$           /$$                                                                  
-| $$  | $$          | $$                                                                  
-| $$  | $$  /$$$$$$ | $$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$   /$$  /$$$$$$ 
-| $$$$$$$$ /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$| $$  | $$ /$$__  $$
-| $$__  $$| $$$$$$$$| $$  \ $$| $$$$$$$$| $$  \__/| $$  \ $$| $$$$$$$$| $$  | $$| $$  \__/
-| $$  | $$| $$_____/| $$  | $$| $$_____/| $$      | $$  | $$| $$_____/| $$  | $$| $$      
-| $$  | $$|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$      |  $$$$$$$|  $$$$$$$|  $$$$$$/| $$      
-|__/  |__/ \_______/|_______/  \_______/|__/       \____  $$ \_______/ \______/ |__/      
-                                                   /$$  \ $$                              
-                                                  |  $$$$$$/                              
-                                                   \______/                               
+print(""" /$$   /$$                       /$$    
+| $$  | $$                      | $$    
+| $$  | $$  /$$$$$$   /$$$$$$$ /$$$$$$  
+| $$$$$$$$ /$$__  $$ /$$_____/|_  $$_/  
+| $$__  $$| $$  \ $$|  $$$$$$   | $$    
+| $$  | $$| $$  | $$ \____  $$  | $$ /$$
+| $$  | $$|  $$$$$$/ /$$$$$$$/  |  $$$$/
+|__/  |__/ \______/ |_______/    \___/  
 """)
 
 path = str(input("[Server] Enter your site directory path:\n>>>"))
-print("\n[Server] initialization...")
-pourcentage = 0
-for i in range(100):
-    pourcentage += 1
-    anim = "█"
-    for i in range(int(pourcentage / 5)):
-        anim += "█"
-    print(f"{pourcentage}% | {anim}", end="\r")
-    sleep(0.025)
+try:
+    port = int(input("[Server] Enter a port:\n>>>"))
+    if 1000 < port < 65535:
+        run(["powershell", "-Command", "python -m pip install -r requirements.txt"])
+        run(["powershell", "-Command", "python -m pip install --upgrade pip"])
+        run(["powershell", "-Command", f"Remove-NetFirewallRule -DisplayName 'Host'"])
+        run(["powershell", "-Command", f"New-NetFirewallRule -DisplayName 'Host' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort {port}"])
+        run(["powershell", "-Command", f"New-NetFirewallRule -DisplayName 'Host' -Profile 'Public' -Direction Inbound -Action Allow -Protocol UDP -LocalPort {port}"])
+        print("\n[Server] I'm ready.")
+        print(f"[Server] Ip: {get('https://api.ipify.org').text}:{port}")
+        class Handler(SimpleHTTPRequestHandler):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, directory=path, **kwargs)
 
-print("[Server] I'm ready.")
-
-class Handler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=path, **kwargs)
-server = HTTPServer
-server(("0.0.0.0", 8080), Handler).serve_forever()
+        HTTPServer(("0.0.0.0", port), Handler).serve_forever()
+    else:
+        print("[Server] The port must be between 1000 and 65535.")
+except ValueError:
+    print("[Server] The chosen port is not valid.")

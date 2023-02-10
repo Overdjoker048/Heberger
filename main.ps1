e946d44d5adbfa4ab18589263e4898f917f77dd5
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2023-02-10 09:26:14 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,36 +1,37 @@
 cls
-set-ExecutionPolicy Unrestricted
-python -m pip install -r requirements.txt
-python -m pip install --upgrade pip
 Remove-NetFirewallRule -DisplayName 'Host'
-cls
+try{
+    Set-ExecutionPolicy Undefined
+    cls
 
-write-host @"
- **      **                    **  
-/**     /**                   /**  
-/**     /**  ******   ****** ******
-/********** **////** **//// ///**/ 
-/**//////**/**   /**//*****   /**  
-/**     /**/**   /** /////**  /**  
-/**     /**//******  ******   //** 
-//      //  //////  //////     //  
+    write-host @"
+     **      **                    **  
+    /**     /**                   /**  
+    /**     /**  ******   ****** ******
+    /********** **////** **//// ///**/ 
+    /**//////**/**   /**//*****   /**  
+    /**     /**/**   /** /////**  /**  
+    /**     /**//******  ******   //** 
+    //      //  //////  //////     //  
 
 "@
 
-if ($path | Test-Path){
-    cd $path
-    try{
-        [int]$port = Read-Host "[Server] Enter a port"
-        if (1000 -lt $port -and $port -lt 65535){
-            New-NetFirewallRule -DisplayName 'Host' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $port
-            New-NetFirewallRule -DisplayName 'Host' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $port
-            "[Server] I'm ready."
-            "[Server] " + (Invoke-WebRequest -uri "https://api.ipify.org/").Content
-            python -m http.server $port
-        }else {
-            "[Server] The port must be between 1000 and 65535."}
-    }catch{
-    "[Server] The chosen port is not valid."}
-}else{
-    "[Server] The path you entered does not exist."
-}
+    [string]$path = Read-Host "[Server] Enter your site directory path"
+
+    if ($path | Test-Path){
+        cd $path
+        try{
+            [int]$port = Read-Host "[Server] Enter a port"
+            if (1000 -lt $port -and $port -lt 65535){
+                New-NetFirewallRule -DisplayName 'Host' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $port
+                New-NetFirewallRule -DisplayName 'Host' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $port
+                "[Server] I'm ready."
+                try{
+                    "[Server] http://" + (Invoke-WebRequest -uri "https://api.ipify.org/").Content + "/"
+                    python -m http.server $port
+                }catch{"[Server] You have no connection."}
+            }else {"[Server] The port must be between 1000 and 65535."}
+        }catch{"[Server] The chosen port is not valid."}
+    }else{"[Server] The path you entered does not exist."}
+}catch{cls
+"[Server] You don't have permission for launch me."}
